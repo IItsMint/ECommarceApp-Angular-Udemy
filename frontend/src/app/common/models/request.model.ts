8f751ddefe7b2e609f8c0b96a7ff8db77330033e
kaan <kaanakman42@gmail.com>
2024-10-30 04:30:32 +0300

    home page structured

--- frontend/src/app/common/models/request.model.ts
+++ frontend/src/app/common/models/request.model.ts
@@ -2,4 +2,6 @@ export class RequestModel{
     pageNumber: number = 1;
     pageSize: number = 10;
     search: string = "";
+    categoryName: string = "Show All";
+    priceFilter: string = "0";
 }
